cce199ea227473e59240075fca716d8829577f31
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-02-09 18:04:09 +0100

    feat: Created to test cases
    Tested: level up, item equip, damage with items

--- RPG Heroes/Program.cs
+++ RPG Heroes/Program.cs
@@ -18,6 +18,7 @@ Armor silkyRobe = new Armor("Spider Queen's silky robe", 1, Slot.Body, ArmorType
 Armor ragRobe = new Armor("rag rope", 1, Slot.Body, ArmorType.Cloth, new HeroAttributes(0, 0, 0));
 Armor hood = new Armor("common hood", 1, Slot.Head, ArmorType.Cloth, new HeroAttributes(0, 0, 1));
 Armor pants = new Armor("pants", 1, Slot.Legs, ArmorType.Cloth, new HeroAttributes(1, 1, 1));
+Armor commonMail = new Armor("common mail", 1, Slot.Body, ArmorType.Mail, new HeroAttributes(2, 1, 0));
 
 List<Hero> heroes = new List<Hero>();
 
@@ -26,15 +27,32 @@ heroes.Add(legolas);
 heroes.Add(mukmuk);
 heroes.Add(thief);
 
-for(int i = 0; i < 10; i++)
+
+//does level up increase their stats correclty
+Console.WriteLine("Level Up test");
+foreach (Hero hero in heroes)
 {
-    mukmuk.LevelUp();
+    hero.Display();
+    hero.LevelUp();
+    hero.Display();
 }
 
-foreach(Hero hero in heroes)
+//test can Heroes equip items correctly
+Console.WriteLine("\nItem equip test");
+foreach (Hero hero in heroes)
 {
     hero.Equip(staffOfWisdom);
     hero.Equip(wandOfPuberty);
-    hero.Display();
+}
+
+//does items and armor affect dmg
+
+Console.WriteLine("\nDamage test");
+foreach(Hero hero in heroes)
+{
+    hero.Damage();
+    hero.Equip(commonAxe);
+    hero.Damage();
+    hero.Equip(commonMail);
     hero.Damage();
 }
